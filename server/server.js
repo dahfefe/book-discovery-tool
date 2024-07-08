91ffa958927951aea30199e44839b8c48b26b5d8
Felix Teng <fsmunozteng@gmail.com>
2024-07-08 01:25:04 -0700

    install apollo at top level

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
