e906840cee8b122eeff9eb0781697eb8bd524ad0
Felix Teng <fsmunozteng@gmail.com>
2024-07-10 04:37:17 -0700

    fully updated User model

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-// import { useMutation } from '@apollo/client';
+import { Form, Button } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
 // import { LOGIN_USER } from '../utils/mutations';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
