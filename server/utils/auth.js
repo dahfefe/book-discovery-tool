53eedabbaff4e703bb3ea744747b8947e3c77f90
Felix Teng <fsmunozteng@gmail.com>
2024-07-12 21:49:27 -0700

    updated backend auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,51 @@
+// Import the jsonwebtoken module
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+// Secret key for signing the JWT. Default to 'mysecretsshhhhh' if not provided in environment variables
+const secret = 'mysecretssshhhhhhh';
+// Token expiration time
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  // Middleware function for authenticating a request
+  authMiddleware: function ({ req }) {
+    // Retrieve the token from the request body, query parameters, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // If the token is in the authorization header, extract the token part
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // If no token is found, return the request object as is
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
     try {
+      // Verify the token using the secret and expiration time
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      // Attach the decoded data to the request object
       req.user = data;
     } catch {
+      // Log an error message if the token is invalid
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // Return the request object
+    return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
+  // Function to sign a JWT with customer information
+  signToken: function ({ email, username, _id }) {
+    // Payload containing the customer's first name, last name, email, and ID
+    const payload = { email, username, _id };
+    // Sign the token with the payload, secret, and expiration time
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
