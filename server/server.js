8a6399c1c091534f39d9469efdc298d7d1dc02d1
Felix Teng <fsmunozteng@gmail.com>
2024-07-12 21:55:27 -0700

    updated backend server.js

--- server/server.js
+++ server/server.js
@@ -20,13 +20,14 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
 
+  // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -34,6 +35,8 @@ const startApolloServer = async () => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
+
+  app.use(routes);
 
   db.once('open', () => {
     app.listen(PORT, () => {
@@ -44,4 +47,4 @@ const startApolloServer = async () => {
 };
 
 // Call the async function to start the server
-  startApolloServer();
+startApolloServer();
